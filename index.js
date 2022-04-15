98e8f05706567a47236be080e0d566534cbe094e
aniketramchandrakoli <95905447+aniketramchandrakoli@users.noreply.github.com>
2022-04-15 16:53:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(body_parser.urlencoded({limit:'50mb', extended: true}));
 app.use(express.static("assets"));
 
 
-mongoose.connect("mongodb+srv://Aniket:<Aniket1234>@cluster0.rogk1.mongodb.net/ecommerce");
+mongoose.connect("mongodb+srv://Aniket:Aniket1234@cluster0.rogk1.mongodb.net/ecommerce");
 const db = mongoose.connection;
 db.on("error", error=> console.log(error));
 db.on("open", ()=> console.log("Connection Established"));
